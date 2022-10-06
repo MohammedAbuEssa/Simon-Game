9270a2018e015f954b4d22c8b533996ccbf4594c
MohammedAbuEssa <mabuissa5@gmail.com>
2022-10-06 20:48:07 +0300

    First commit

---
+++ game.js
@@ -0,0 +1,131 @@
+// var buttonColours = ["red", "blue", "green", "yellow"];
+// var userClickedPattern=[]
+// function nextSequence() {
+//
+//   var randomNumber = Math.floor(Math.random() * 4);
+//   var randomChosenColour = buttonColours[randomNumber];
+//   gamePattern.push(randomChosenColour);
+//
+//   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+//
+//   var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+//   audio.play();
+// }
+//
+//
+// $( ".btn" ).click(function() {
+// var userChosenColour =$(this).attr("id");
+// userClickedPattern.push(userChosenColour);
+// // console.log(userClickedPattern);
+// });
+//
+// function playSound(name){
+// nextSequence(name);
+// }
+
+
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+//You'll need a way to keep track of whether if the game has started or not, so you only call nextSequence() on the first keypress.
+var started = false;
+
+//2. Create a new variable called level and start at level 0.
+var level = 0;
+
+//1. Use jQuery to detect when a keyboard key has been pressed, when that happens for the first time, call nextSequence().
+$(document).keypress(function() {
+  if (!started) {
+
+    //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
+
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+
+function checkAnswer(currentLevel) {
+
+  //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+    console.log("success");
+
+    //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+    if (userClickedPattern.length === gamePattern.length) {
+
+      //5. Call nextSequence() after a 1000 millisecond delay.
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+
+    }
+
+  } else {
+
+    console.log("wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+    startOver();
+
+  }
+
+}
+
+function nextSequence() {
+  userClickedPattern = [];
+
+  //4. Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
+  level++;
+
+  //5. Inside nextSequence(), update the h1 with this change in the value of level.
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function() {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function startOver() {
+
+  //3. Inside this function, you'll need to reset the values of level, gamePattern and started variables.
+  level = 0;
+  gamePattern = [];
+  started = false;
+}
